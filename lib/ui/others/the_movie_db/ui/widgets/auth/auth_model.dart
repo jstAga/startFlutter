18acfe19878bbbb2e20cfeea02bf04e331544407
Aktan <86038270+jstAga@users.noreply.github.com>
2023-07-05 17:23:53 +0600

    add request validation, saving sessionId

--- lib/ui/others/the_movie_db/ui/widgets/auth/auth_model.dart
+++ lib/ui/others/the_movie_db/ui/widgets/auth/auth_model.dart
@@ -1,15 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:start_flutter/ui/main_navigation/main_navigation.dart';
+import 'package:start_flutter/ui/others/the_movie_db/data/local/data_provider/session_data_provider.dart';
 import 'package:start_flutter/ui/others/the_movie_db/data/remote/api_client/api_client.dart';
 
 class AuthModel extends ChangeNotifier {
   final _apiClient = ApiClient();
+  final _sessionDataProvider = SessionDataProvider();
   final usernameTextController = TextEditingController();
   final passwordTextController = TextEditingController();
   String? _errorMessage;
   bool _isAuthLoading = false;
 
   bool get canStartAuth => !_isAuthLoading;
+
   bool get isAuthInProgress => _isAuthLoading;
 
   String? get errorMessage => _errorMessage;
@@ -17,7 +22,7 @@ class AuthModel extends ChangeNotifier {
   Future<void> auth(BuildContext context) async {
     final username = usernameTextController.text;
     final password = passwordTextController.text;
-    if(username.isEmpty || password.isEmpty) {
+    if (username.isEmpty || password.isEmpty) {
       _errorMessage = "Username or password is empty";
       notifyListeners();
       return;
@@ -25,10 +30,23 @@ class AuthModel extends ChangeNotifier {
     _errorMessage = null;
     _isAuthLoading = true;
     notifyListeners();
-    final sessionId = await _apiClient.auth(username: username, password: password);
+    String? sessionId;
+    try {
+      sessionId = await _apiClient.auth(username: username, password: password);
+    } catch (e) {
+      _errorMessage = "Username or password is incorrect";
+    }
     _isAuthLoading = false;
-    notifyListeners();
-    // Navigator.pushNamed(context, MainNavigationRoutesNames.homeMovieDb);
+    if (errorMessage != null) {
+      notifyListeners();
+      return;
+    } else if (sessionId == null) {
+      _errorMessage == "Unknown error try again";
+      notifyListeners();
+      return;
+    }
+    await _sessionDataProvider.setSessionId(sessionId);
+        Navigator.pushNamed(context, MainNavigationRoutesNames.homeMovieDb);
   }
 }
 
